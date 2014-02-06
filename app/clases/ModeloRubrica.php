0eaaf2fa7f994bc91cd01e8cf0139a35c83549ca
halan-loma <halan_loma@hotmail.com>
2014-02-05 20:55:12 -0500

    listarModeloRubricaPorCriterio
    Ya  era hora XD

--- app/clases/ModeloRubrica.php
+++ app/clases/ModeloRubrica.php
@@ -16,17 +16,33 @@ class ModeloRubrica{
 
 	}
 
-	public function listarModeloRubricaPorDocente(){
 
-		$query = "SELECT * FROM modelorubrica";
-		$resultado = $this->conexion->realizarConsulta($query);
-		$a = array('modelos'=>$resultado);
+	//Aun falta parametros 
+	public function listarModeloRubricaPorCriterios(){
 
+		$query1 = "SELECT * FROM modelorubrica where idModeloRubrica = 1";
+		$resultado1 = $this->conexion->realizarConsulta($query1);
+
+		$query2 = "SELECT a.CriterioEvaluacion_idCriterioEvaluacion,r.definicionResultadoAprendizaje ,c.descripcionCriterioEvaluacion  FROM asignacioncriterioevaluacion as a
+		inner join criterioevaluacion  as c on c.idCriterioEvaluacion = a.CriterioEvaluacion_idCriterioEvaluacion 
+		inner join resultadoaprendizaje as r on r.idResultadoAprendizaje =c.ResultadoAprendizaje_idResultadoAprendizaje 
+		where ModeloRubrica_idModeloRubrica = 1";
+		$resultado2 = $this->conexion->realizarConsulta($query2);
+
+		//Probando
+
+
+		//
+
+		$a = array('modelo'=>$resultado1,'criterio'=>$resultado2);
 		$resultadoJson = $this->conexion->convertirJson($a);
 		return $resultadoJson;
+	}
+
+	public function listarModeloRubricaPorDocente(){
 	}
 
 }
 
 $objetoModeloRubrica = new ModeloRubrica();
-echo $objetoModeloRubrica->listarModeloRubricaPorDocente();
+echo $objetoModeloRubrica->listarModeloRubricaPorCriterios();
