a66e972dcc20e6cf1b01bb32d4bd393dcc0ced04
arocutipa <sk24d@yahoo.com.mx>
2014-02-07 18:56:50 -0500

    alumno

---
+++ app/clases/Alumno.php
@@ -0,0 +1,32 @@
+<?php
+
+header('Content-type: application/json');
+
+require_once('Conexion.php');
+
+class Alumno{
+
+	private $conexion;
+
+	public function __construct(){
+		$this->conexion = ConexionFactory::obtenerConexion('sqlserver','192.168.1.38','sa','123cuatro');
+	}
+
+	public function listarAlumnoPorCursoSemetre(){
+
+		$semestre = $_POST['txtSemetre'];
+		$curso = $_POST['txtCurso'];
+		$query = "select cu.DesCurso, p.NomPer, s.Semestre from carga as c
+		inner join PERSONA as p on p.CodPer = c.codper
+		inner join curso as cu on cu.idcurso = c.idcurso
+		inner join SEMESTRE as s on s.IdSem = c.idsem 
+		where c.idsem ='".$semestre."' and c.idcurso ='".$curso."' and p.CodEstamento=10";
+		$resultado = $this->conexion->realizarConsulta($query);
+		$resultadoJson = $this->conexion->convertirJson($resultado);
+		return $resultadoJson;
+	}
+
+}
+
+$objetoRA = new Alumno();
+echo $objetoRA->listarAlumnoPorCursoSemetre();
