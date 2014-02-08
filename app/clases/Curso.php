58c180c2413f5b47c421e64df8614fd8674b9991
arocutipa <sk24d@yahoo.com.mx>
2014-02-07 19:46:15 -0500

    Curso activo por semestre

--- app/clases/Curso.php
+++ app/clases/Curso.php
@@ -4,7 +4,7 @@ header('Content-type: application/json');
 
 require_once('Conexion.php');
 
-class Docente{
+class Curso{
 
 	private $conexion;
 
---
+++ app/modulos/Curso.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Content-type: application/json');
+
+require_once('../clases/Curso.php');
+
+$metodo = $_GET['metodo'];
+
+$Curso = new Curso();
+
+switch($metodo){
+	case 'listarCursoActivoSemestre' : echo $Curso->listarCursoActivoSemestre();break;
+}
