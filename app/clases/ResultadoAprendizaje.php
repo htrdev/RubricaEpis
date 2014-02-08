fe79f9cff5216a7f4399a5edd7ced6625fcb27d0
htrdev <htrdev@live.com>
2014-02-07 21:29:04 -0500

    Json de listarResultadoAprendizaje implementado

--- app/clases/ResultadoAprendizaje.php
+++ app/clases/ResultadoAprendizaje.php
@@ -12,67 +12,28 @@ class ResultadoAprendizaje{
 		$this->conexion = ConexionFactory::obtenerConexion('mysql','192.168.1.35','htrdev','12345');
 	}
 
+	public function listarCriterioAprendizaje($idResultadoAprendizaje){
+		$query = "SELECT  c.idCriterioEvaluacion,c.descripcionCriterioEvaluacion,c.ResultadoAprendizaje_idResultadoAprendizaje 
+					FROM criterioevaluacion as c WHERE c.ResultadoAprendizaje_idResultadoAprendizaje = '".$idResultadoAprendizaje."'";
+		return $this->conexion->realizarConsulta($query);
+	}
 
 	public function listarResultadoAprendizaje(){
 
-		$query1 = "SELECT  r.idResultadoAprendizaje, r.definicionResultadoAprendizaje  FROM resultadoaprendizaje as r";
-		$resultado1 = $this->conexion->realizarConsulta($query1);
-
-		$query2 = "SELECT  c.idCriterioEvaluacion,c.descripcionCriterioEvaluacion,c.ResultadoAprendizaje_idResultadoAprendizaje   FROM criterioevaluacion as c";
-		$resultado2 = $this->conexion->realizarConsulta($query2);
-
-		$f=0;
-		$c=0;
-
-		$obj1[]= "";
-	
-
-		foreach($resultado1 as $key1){
-
-				foreach($resultado2 as $key2){
-    			
-    			 if($key1['idResultadoAprendizaje']==$key2['ResultadoAprendizaje_idResultadoAprendizaje'])
-    			 { 			 	
-    			 		$obj1[$f++]= $key2['descripcionCriterioEvaluacion'];
-    			 		//$obj2[$c]= $key1['definicionResultadoAprendizaje'];	
-    			 } 
-    			 else
-    			 {
- 			 	
-    			 }
-
-    			 /*
-
-    			 if($key1['idResultadoAprendizaje']==$key2['ResultadoAprendizaje_idResultadoAprendizaje'])
-    			 {
-    			 	
-    			 	if($f==0){
-    			 		$obj[$f][$c]= $key1['definicionResultadoAprendizaje'];
-    			 		$obj[$f][++$c]= $key2['descripcionCriterioEvaluacion'];
-    			 	}
-    			 	else{
-    			 		$obj[$f][++$c]= $key2['descripcionCriterioEvaluacion'];
-    			 	}
-    			 	
-    			 	$f++;
-    			 }
-    			 else
-    			 {
-    			 		$f=0;
-    			 		$c=0;
-    			 }
-
-    			 */
-    		} 		 
+		$query = "SELECT  r.idResultadoAprendizaje, r.definicionResultadoAprendizaje  FROM resultadoaprendizaje as r";
+		$resultadosAprendizaje = $this->conexion->realizarConsulta($query);
+		$resultado = array();
+		$contadorResultado = 0;
+		foreach($resultadosAprendizaje as $resultadoAprendizaje){
+			$idResultadoAprendizaje = $resultadoAprendizaje["idResultadoAprendizaje"];
+			$criteriosAprendizaje = $this->listarCriterioAprendizaje($idResultadoAprendizaje);
+			$resultado[$contadorResultado] = 
+				array("tituloResultadoAprendizaje"=>$resultadoAprendizaje["definicionResultadoAprendizaje"],
+					"criteriosEvaluacion"=>$criteriosAprendizaje); 
+			$contadorResultado++;
 		}
-
-
-
-
-		$a = array('Criterios'=>$obj1);
-		$resultadoJson = $this->conexion->convertirJson($a);
+		$resultadoJson = $this->conexion->convertirJson($resultado);
 		return $resultadoJson;
-				
 	}
 
 
