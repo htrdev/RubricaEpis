1f6d94128d852ed25149b2584e68d41f5cc3980b
arocutipa <sk24d@yahoo.com.mx>
2014-02-07 19:27:34 -0500

    DocenteActivo

---
+++ app/clases/docente.php
@@ -0,0 +1,30 @@
+<?php
+
+header('Content-type: application/json');
+
+require_once('Conexion.php');
+
+class Docente{
+
+	private $conexion;
+
+	public function __construct(){
+		$this->conexion = ConexionFactory::obtenerConexion('sqlserver','192.168.1.38','sa','123cuatro');
+	}
+
+	public function listarDocenteActivo(){
+
+		$activo = $_POST['txtActivo'];
+		$query = "select  p.NomPer, p.ApepPer, p.ApemPer, s.Semestre from carga as c
+		inner join PERSONA as p on p.CodPer = c.codper
+		inner join curso as cu on cu.idcurso = c.idcurso
+		inner join SEMESTRE as s on s.IdSem = c.idsem where p.CodEstamento =1 and s.Activo='".$activo."'";
+		$resultado = $this->conexion->realizarConsulta($query);
+		$resultadoJson = $this->conexion->convertirJson($resultado);
+		return $resultadoJson;
+	}
+
+}
+
+$objetoRA = new Docente();
+echo $objetoRA->listarDocenteActivo();
