9224d924fd990de18afa5970ac387890597a9ae1
halan-loma <halan_loma@hotmail.com>
2014-02-18 20:52:19 -0500

    modeloRubrica
    modelo

--- app/clases/ModeloRubrica.php
+++ app/clases/ModeloRubrica.php
@@ -1,49 +1,31 @@
 <?php
 
 header('Content-type: application/json');
-
 require_once('Conexion.php');
 
 class ModeloRubrica{
 
-	private $conexion;
+	private $conexionMysql;
+	private $conexionSqlServer;
 
 	public function __construct(){
-		$this->conexion = ConexionFactory::obtenerConexion('mysql','localhost','htrdev','12345');
+		$this->conexionMysql = ConexionFactory::obtenerConexion('mysql','192.168.1.35','htrdev','12345');
+		$this->conexionSqlServer = ConexionFactory::obtenerConexion('sqlserver','192.168.1.38','sa','123cuatro');
 	}
 
-	public function agregarModeloRubrica(){
-
+	public function listarRubricasPorPersona(){
+		//echo $this->conexion->obtenerVariableSesion("CodPer");
+		echo $this->conexionMysql->obtenerVariableSesion("CodPer");
 	}
-
-	public function modificarModeloRubrica(){
-	}
-	//Aun falta parametros 
-	public function listarModeloRubricaPorCriterios(){
-
-		$query1 = "SELECT * FROM modelorubrica where idModeloRubrica = 1";
-		$resultado1 = $this->conexion->realizarConsulta($query1);
-
-		$query2 = "SELECT a.CriterioEvaluacion_idCriterioEvaluacion,r.definicionResultadoAprendizaje ,c.descripcionCriterioEvaluacion  FROM asignacioncriterioevaluacion as a
-		inner join criterioevaluacion  as c on c.idCriterioEvaluacion = a.CriterioEvaluacion_idCriterioEvaluacion 
-		inner join resultadoaprendizaje as r on r.idResultadoAprendizaje =c.ResultadoAprendizaje_idResultadoAprendizaje 
-		where ModeloRubrica_idModeloRubrica = 1";
-		$resultado2 = $this->conexion->realizarConsulta($query2);
-		//Probando
-		//
-		$a = array('modelo'=>$resultado1,'criterio'=>$resultado2);
-		$resultadoJson = $this->conexion->convertirJson($a);
-		return $resultadoJson;
-	}
-
-	public function listarModeloRubricaPorDocente(){
-	}
-
-
-	
-
 
 }
 
-$objetoModeloRubrica = new ModeloRubrica();
-echo $objetoModeloRubrica->listarModeloRubricaPorCriterios();
+$objeto = new ModeloRubrica();
+echo  $objeto->listarRubricasPorPersona();
+ 
+
+
+
+
+
+
