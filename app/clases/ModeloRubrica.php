97037cbb599ebe64e5b2e65a2e6a68d842df6642
arocutipa <sk24d@yahoo.com.mx>
2014-02-19 21:06:27 -0500

    modelorubrica

--- app/clases/ModeloRubrica.php
+++ app/clases/ModeloRubrica.php
@@ -18,6 +18,42 @@ class ModeloRubrica{
 		echo $this->conexionMysql->obtenerVariableSesion("CodPer");
 	}
 
+	public function listarUltimoPrimaryKey($nombreCampoID,$tabla){
+		$query1="select LAST_INSERT_ID(".$nombreCampoID.") from ".$tabla;
+		$resultadoQuery = $this->conexion->realizarConsulta($query1,true);
+		$query2.="select LAST_INSERT_ID()";
+		$resultadoQuery2 = $this->conexion->realizarConsulta($query2,true);
+		return $resultadoQuery2[0]['LAST_INSERT_ID()'];
+	}
+
+	public function agregarModeloRubrica($agregarModeloRubrica){
+
+		$queryInsertarModeloRubrica="insert into modelorubrica as m 
+		(m.Curso_idCurso, m.Semestre_idSemestre, m.fechaInicioRubrica,m.fechaFinalRubrica,
+		 m.Docente_Persona_idPersona,m.calificacionRubrica)
+		values ('".$agregarModeloRubrica["Curso_idCurso"]."'
+				,'".$agregarModeloRubrica["Semestre_idSemestre"]."'
+				,'".$agregarModeloRubrica["fechaInicioRubrica"]."'
+				,'".$agregarModeloRubrica["fechaFinalRubrica"]."'
+				,'".$agregarModeloRubrica["Docente_Persona_idPersona"]."'
+				,'".$agregarModeloRubrica["calificacionRubrica"]."')";
+
+		$queryAgregarModeloRubrica= $this->conexion->realizarConsulta($query,false);
+
+		$idResultadoAprendizaje = $this->listarUltimoPrimaryKey('idModeloRubrica','criterioevaluacion');
+
+
+		
+		$queryAsignarCriterioEvaluacion="insert into a.ModeloRubrica_idModeloRubrica, a.CriterioEvaluacion_idCriterioEvaluacion
+ 		from asignacioncriterioevaluacion as a values ('".$agregarModeloRubrica["Curso_idCurso"]."'
+ 														,'".$agregarModeloRubrica["Curso_idCurso"]."')";
+
+
+
+	}
+
+	
+
 }
 
 $objeto = new ModeloRubrica();
