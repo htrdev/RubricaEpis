ecedb4802166cbf0ac96791d08116db88216011a
arocutipa <sk24d@yahoo.com.mx>
2014-03-01 09:41:19 -0500

    Listar Resultado de Rubrica por Modelo Rubrica

--- app/modulos/ResultadoRubrica.php
+++ app/modulos/ResultadoRubrica.php
@@ -8,10 +8,11 @@ $json = json_decode(file_get_contents("php://input"),true);
 $metodo = $json['metodo'];
 $idModeloRubrica= $json['idModeloRubrica'];
 
-$ResultadoRubrica = new ResultadoRubrica();
+$resultadoRubrica = ResultadoRubrica::obtenerObjeto();
+
 
 switch($metodo){
 
-	case 'listarResultadoRubricaPorIDModeloRubrica' : echo $ResultadoRubrica->listarResultadoRubricaPorIDModeloRubrica($idModeloRubrica);break;
-	case 'listarResultadoRubricaPorcionRubricaAsignada' : echo $ResultadoRubrica->listarResultadoRubricaPorcionRubricaAsignada($idModeloRubrica);break;
+	case 'listarResultadoRubricaPorIDModeloRubrica' : echo $resultadoRubrica->listarResultadoRubricaPorIDModeloRubrica($idModeloRubrica);break;
+	case 'listarResultadoRubricaPorcionRubricaAsignada' : echo $resultadoRubrica->listarResultadoRubricaPorcionRubricaAsignada($idModeloRubrica);break;
 }
